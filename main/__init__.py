5aa3d9247b027b7001ad3d190df53eb2069c124f
harshit-bit10 <109902163+harshit-bit10@users.noreply.github.com>
2025-03-16 16:35:16 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = 4857766
+API_HASH = "6c3c6facf5598a4b318e138f8c407028"
+BOT_TOKEN = "7038431984:AAFHdIhCf33k-eGhgTDWyM1uuC0gd7ywS9M"
+SESSION = "BQFTZuMAo_EtgtYPpUclUSUjSCOcghhn0kRo4YUMswdUf3_LrgYuuIKfAIQUH94liIw9j2FwI2t7iu8fbqJTemrtAHEwBHCQw7IGbyc3iD_K-UTN21ZEkm5KGFsCGcc6aNbjPSiLkugGkrD9h_FX81AwBhM_itGs1mwl5IlxPzSkcfttPWngJtJ4L5aUqms9cKUy4S76_9cK9niojlUNYCUOWBurY2l-QOCzf27_wOPboNQ_UA_ZWojmy6a8NMNBiOsNWnKzg83PWn38KjELNHhoNmarcngiyiqFdXNQifyBCyt-FdaXeCjHuJuYG5D6xR-QcVSW3BEozFm0lXs6XHRmASigAAAAA5g3ELAA"
+FORCESUB = "SharkToonsIndia"
+AUTH = 6066102279
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
